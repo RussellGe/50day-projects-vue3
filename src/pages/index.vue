05ba44bd980c24f1a9d7ea30ad8732a13027ca64
GeYuan <120463275@qq.com>
2022-04-28 17:23:33 +0800

    fix: init project

---
+++ src/pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <div>
+      <RouterLink
+        class="m-3 text-sm btn"
+        to='/expandingCards'
+      >
+        ExpandingCard
+      </RouterLink>
+    </div>
+  </div>
+</template>
